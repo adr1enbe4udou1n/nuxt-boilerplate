d595b940e32338cb37182c9b3c8fc96bab0f782f
Adrien Beaudouin <adrien.beaudouin@outlook.com>
2018-11-24 19:14:53 +0100

    up

--- .eslintrc.js
+++ .eslintrc.js
@@ -21,6 +21,7 @@ module.exports = {
     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     'vue/attributes-order': 'off',
+    'vue/component-name-in-template-casing': 'off',
     'vue/html-self-closing': ['error', {
       html: {
         void: 'always',
@@ -31,7 +32,9 @@ module.exports = {
       math: 'never'
     }],
     'vue/max-attributes-per-line': 'off',
+    'vue/multiline-html-element-content-newline': 'off',
     'vue/no-unused-vars': 'off',
-    'vue/no-v-html': 'off'
+    'vue/no-v-html': 'off',
+    'vue/singleline-html-element-content-newline': 'off'
   }
 }
